73ff73656d5f2a78bbb91aa3080157655d16c18d
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-02-15 10:39:46 +0500

    fix

--- OOP_lab_1.cpp
+++ OOP_lab_1.cpp
@@ -5,32 +5,35 @@ using namespace std;
 class Dog {
 public:
     string name;
+    string breed;
+    int dog_age;
+    
     void bark() {
         cout << name << " says Woof!" << endl;
     }
-    string breed;
-    int dog_age;
-        cout << "Breed: " << pet.breed << endl;
-        cout << "Age: " << pet.dog_age << endl;
-
 };
 
 class Person {
 public:
     string name;
-
     Dog pet;
+
     void introducePet() {
         cout << "My dog's name is " << pet.name << endl;
+        cout << "Breed: " << pet.breed << endl;
+        cout << "Age: " << pet.dog_age << endl;
         pet.bark();
     }
 };
 
 int main() {
-    Person personl;
-    personl.name = "Alice";
-    personl.pet.name = "Buddy";
-    personl.pet.dog_age = 3; 
-    personl.introducePet();
+    Person person1;
+    person1.name = "Alice";
+    person1.pet.name = "Buddy";
+    person1.pet.breed = "Labrador";
+    person1.pet.dog_age = 3;
+    
+    person1.introducePet();
+
     return 0;
 }
