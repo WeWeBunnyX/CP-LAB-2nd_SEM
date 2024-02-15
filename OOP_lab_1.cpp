9b5c8f7b33de903482ccf9cad48a8df3cd6a907b
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-02-15 10:17:30 +0500

    commit

--- OOP_lab_1.cpp
+++ OOP_lab_1.cpp
@@ -1,1 +1,19 @@
+class Dog {
+public:
+string name; void bark() {
+cout << name << " says Woof!" « endl;
+}
+};
+class Person { public:
+string name; Dog pet;
+void introducePet() {
+cout « "My dog's name is " « pet.name « endl; pet.bark();
+}
+};
+int main() {
+Person personl;
+personl.name = "Alice";
+personl.pet.name = "Buddy"; personl.introducePet();
+return 0;
+}
 
