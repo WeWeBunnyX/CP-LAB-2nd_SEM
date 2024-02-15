60268834d9a34c6d500ab52a270cab7782e5f7f8
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-02-15 10:25:00 +0500

    add

--- OOP_lab_1.cpp
+++ OOP_lab_1.cpp
@@ -1,27 +1,4 @@
-class Dog {
-public:
-string name; void bark() {
-cout << name << " says Woof!" « endl;
-}
-};
-
-class Person { public:
-string name;
-string breed;
-int dog_age;
-
-Dog pet;
-void introducePet() {
-cout « "My dog's name is " « pet.name « endl; 
-cout<<"Breed: "<<pet.breed<<endl;
-cout<<"Age: " <<pet.age<<endl;
-
-pet.bark();
-}
-};
-
-
-
+#include <iostream> 
 
 int main() {
 Person personl;
@@ -31,3 +8,38 @@ personl.introducePet();
 return 0;
 }
 
+#include <iostream>
+using namespace std;
+
+class Dog {
+public:
+    string name;
+    void bark() {
+        cout << name << " says Woof!" << endl;
+    }
+};
+
+class Person {
+public:
+    string name;
+    string breed;
+    int dog_age;
+
+    Dog pet;
+    void introducePet() {
+        cout << "My dog's name is " << pet.name << endl;
+        cout << "Breed: " << pet.breed << endl;
+        cout << "Age: " << pet.dog_age << endl;
+
+        pet.bark();
+    }
+};
+
+int main() {
+    Person personl;
+    personl.name = "Alice";
+    personl.pet.name = "Buddy";
+    personl.pet.dog_age = 3; 
+    personl.introducePet();
+    return 0;
+}
