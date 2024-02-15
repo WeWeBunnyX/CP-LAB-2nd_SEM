71b6935cd48143892eca273d86e99b74cddedcee
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-02-15 10:22:04 +0500

    Extend Dog

--- OOP_lab_1.cpp
+++ OOP_lab_1.cpp
@@ -4,16 +4,30 @@ string name; void bark() {
 cout << name << " says Woof!" « endl;
 }
 };
+
 class Person { public:
-string name; Dog pet;
+string name;
+string breed;
+int dog_age;
+
+Dog pet;
 void introducePet() {
-cout « "My dog's name is " « pet.name « endl; pet.bark();
+cout « "My dog's name is " « pet.name « endl; 
+cout<<"Breed: "<<pet.breed<<endl;
+cout<<"Age: " <<pet.age<<endl;
+
+pet.bark();
 }
 };
+
+
+
+
 int main() {
 Person personl;
 personl.name = "Alice";
-personl.pet.name = "Buddy"; personl.introducePet();
+personl.pet.name = "Buddy"; 
+personl.introducePet();
 return 0;
 }
 
