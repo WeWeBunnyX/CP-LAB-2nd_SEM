b324a1915cd5c005007b735ce92f2905634f7d8e
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-02-25 14:18:02 +0500

    Update main.cpp

--- Lab 2/Lab Tasks/main.cpp
+++ Lab 2/Lab Tasks/main.cpp
@@ -1,4 +1,21 @@
 #include "Animal.h"
+
+void Lion::makeSound() {
+    cout << "Roar!" << endl;
+}
+void Elephant::makeSound() {
+    cout << "Trumpet!" << endl;
+}
+void Monkey::makeSound() {
+    cout << "Ooh ooh aah aah!" << endl;
+}
+void Bird::makeSound() {
+    cout << "Chirp!" << endl;
+}
+void hearSound(Animal &animal) {
+    animal.makeSound();
+}
+
 int main() {
     Lion lion;
     Elephant elephant;
